88bd0deaecfb5e9c411418658f6de355a6af2d27
gyaanadda <gyaanaddaaap@gmail.com>
2025-04-18 15:29:52 +0530

    refactor: favicon change and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import LandingPage from "@/components/LandinPage";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +14,21 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <section className="scroll-smooth">
-      <LandingPage />
-    </section>
+    <>
+      <Head>
+        <title>Gyaan Adda</title>
+        <meta name="title" content="Gyaan Adda" />
+        <meta
+          name="description"
+          content="Gyan Adda is a collaborative space for learners, mentors, and creators to exchange ideas, insights, and knowledge across a wide range of topics — from tech and career to wellness and more."
+        />
+        <meta />
+        <link rel="canonical" href="https://www.gyaanaddaapp.com/" />
+      </Head>
+
+      <section className="scroll-smooth">
+        <LandingPage />
+      </section>
+    </>
   );
 }
